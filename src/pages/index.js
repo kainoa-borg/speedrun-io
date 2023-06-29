6a2d1d3094670cc846ed58dd04f035312a4042fa
kaiborg <kainoa.accs@gmail.com>
2023-06-28 14:30:06 -1000

    Remove forwardRef

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,14 +44,14 @@ const getGameDetails = (gameID, setGameData, setLoading) => {
   })
 }
 
-const GameListing = React.forwardRef((props, ref) => {
+const GameListing = (props) => {
   return (
     <div className='flex space-x-1 pb-2'>
       <h2 className='bg-slate-500 p-4 grow'>{props.gameName}</h2>
       <button className='bg-neutral-600 p-4 md:w-[25%] w-[30%]' onClick={() => {getGameDetails(props.gameID, props.setGameData, props.setLoading)}}>Get Details</button>
     </div>
   );
-})
+}
 
 const convertTime = (time) => {
   let ms = String(Math.floor(time % 1 * 10));
