0580e93f8bf6f0590cdc397c42d7777ff2696ae4
kaiborg <kainoa.accs@gmail.com>
2023-06-28 14:26:36 -1000

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,193 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter, M_PLUS_1 } from 'next/font/google'
+import axios from 'axios'
+import {AnimatePresence, motion, stagger} from 'framer-motion'
+import { useEffect, useState } from 'react'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const searchGames = (nameQuery, setGameSearchList, setLoading) => {
+  console.log('searching...');
+  setLoading(true);
+  axios.get('https://www.speedrun.com/api/v1/games?name='+nameQuery).then((response) => {
+    setGameSearchList(response.data.data);
+    setLoading(false);
+  }).catch((error) => {
+    console.log(error);
+    setLoading(false);
+  })
+}
+
+const getGameDetails = (gameID, setGameData, setLoading) => {
+  console.log('searching...')
+  setLoading(true);
+  axios.get('https://www.speedrun.com/api/v1/games/'+gameID+'').then((response_game) => {
+    axios.get('https://www.speedrun.com/api/v1/games/'+gameID+'/records?miscellaneous=no&scope=full-game&top=50').then((response_runs) => {
+      response_game.data.data['runs'] = response_runs.data.data[0].runs
+      let cat = response_runs.data.data[0].runs[0].run.category;
+      axios.get('https://www.speedrun.com/api/v1/categories/'+cat).then((response_cat) => {
+        response_game.data.data['runs'].category = response_cat.data.data.name;
+        setGameData(response_game.data.data);
+        setLoading(false);
+      }).catch((error) => {
+        console.log(error);
+        setLoading(false)
+      })
+    }).catch((error) => {
+      console.log(error);
+      setLoading(false);
+    })
+  }).catch((error) => {
+    console.log(error);
+    setLoading(false);
+  })
+}
+
+const GameListing = React.forwardRef((props, ref) => {
+  return (
+    <div className='flex space-x-1 pb-2'>
+      <h2 className='bg-slate-500 p-4 grow'>{props.gameName}</h2>
+      <button className='bg-neutral-600 p-4 md:w-[25%] w-[30%]' onClick={() => {getGameDetails(props.gameID, props.setGameData, props.setLoading)}}>Get Details</button>
+    </div>
+  );
+})
+
+const convertTime = (time) => {
+  let ms = String(Math.floor(time % 1 * 10));
+  let s = String(Math.floor(time % 60));
+  let m = String(Math.floor(time / 60));
+  let h = String(Math.floor(time / 60 / 60));
+  if (ms.length < 2) 
+    ms = '0'+ms;
+  if (s.length < 2) 
+    s = '0'+s;
+  if (m.length < 2) 
+    m = '0'+m;
+  if (h.length < 2) 
+    h = '0'+h;
+  return h + 'h:' + m + 'm:' + s + 's:' + ms + 'ms';
+}
+
+const list = {
+  hidden: {
+    opacity: 0, 
+    transition: {
+      when: 'afterChildren',
+    }
+  }, 
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+    }
+  }
+}
+
+const item = {
+  hidden: i => ({
+    opacity: 0, y:-10,
+    transition: {
+      duration: 1,
+      delay: i * .1
+    }
+  }), 
+  visible: i => ({
+    opacity: 1, y: 0,
+    transition: {
+      duration: 1,
+      delay: i * .1
+    }
+  })
+}
+
 export default function Home() {
+
+  const [nameQuery, setNameQuery] = useState('');
+  const [gameSearchList, setGameSearchList] = useState([]);
+  const [gameData, setGameData] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (nameQuery.length < 1) {
+      setGameSearchList([]);
+    }
+  }, [nameQuery])
+
+  useEffect(() => {
+    if (gameSearchList.length > 0) {
+      setGameData();
+    }
+  }, [gameSearchList])
+
+  useEffect(() => {
+    setGameSearchList([]);
+  }, [gameData])
+
+  const handleKeyDown = (event) => {
+    if (event.key == 'Enter') {
+      setGameSearchList([]); 
+      searchGames(nameQuery, setGameSearchList, setLoading);
+    }
+  }
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`min-h-screen md:p-24 p-12 ${inter.className}`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <AnimatePresence>
+      <div className='flex'>
+        <div className='md:w-[50%] w-[100%]'>
+          <input 
+            className='p-4 text-black'
+            onChange={(event) => {setNameQuery(event.target.value);}}
+            onKeyDown={handleKeyDown}
+            value={nameQuery}
+            placeholder={'Enter game name:'}
+          ></input>
+          <button 
+            className='p-4 mb-4 bg-slate-500 text-white'
+            onClick={() => {setGameSearchList([]); searchGames(nameQuery, setGameSearchList, setLoading)}}  
+          >Get Games</button>
+
+          {loading ? <div><motion.p key='motion-p' initial='hidden' animate='visible' exit='hidden' variants={item}>loading...</motion.p></div> : <></>}
+
+          <motion.div initial='hidden' animate='visible' exit='hidden' variants={list}>
+          {gameSearchList.map((game, i) => {
+            return (
+              <motion.div layout key={game.id} custom={i} variants={item}>
+                <GameListing gameName={game.names.international} gameID={game.id} setLoading={setLoading} setGameData={setGameData}/>
+              </motion.div>
+            )
+          })}
+          </motion.div>
+
+          {
+            gameData ?
+            <motion.div key='detail-container' className='' initial='hidden' animate='visible' exit='hidden' variants={list}>
+              <motion.h2 key='detail-h2' variants={item} className='text-3xl pb-4'>{gameData.names.international}</motion.h2>
+              <motion.div key='detail-div' variants={item}>
+                <img src={gameData.assets['cover-small'].uri}/>
+              </motion.div>
+              <motion.h3 key='detail-runs-header' variants={item} className='text-2xl pt-6'>Best Times:</motion.h3>
+              <motion.h3 key='detail-runs-header' variants={item} className='text-2xl pb-2'>'{gameData.runs.category}'</motion.h3>
+              <motion.ul key='detail-runs' variants={list} className=''>
+                {gameData.runs.slice(0, 5).map((run, i) => {
+                  return(
+                  <motion.li key={run.run.id} variants={item} className='text-white'>{i+1}: {convertTime(run.run.times.primary_t)}</motion.li>
+                  )
+                })}
+              </motion.ul>
+            </motion.div>
+            :
+            <></>
+          }
+        </div>
+        <div className='md:w-[50%] w-[100%]'>
+
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    </AnimatePresence>      
     </main>
   )
 }
